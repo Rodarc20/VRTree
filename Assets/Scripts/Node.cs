8d9c03e6d612da61a67b92d2acaa48a8b72e57f7
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2017-02-09 16:46:56 -0500

    Agregando anotaciones

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Xml;
+using System.Xml.Serialization;
 public class Node : MonoBehaviour {
 
+    [XmlAttribute("id")]
     int id;
+    [XmlElementAttribute("valid")]
     bool valid;
+    [XmlElementAttribute("x-coordinate")]
     float x;
+    [XmlElementAttribute("y-coordinate")]
     float y;
+    [XmlElementAttribute("url")]
     string url;
     //tiene un array de escalares y un array de labels, por ahora seran omitidos
     //quiz estos no deban ser nodes, sino, solo nobres los ides;
@@ -26,5 +32,6 @@ public class Node : MonoBehaviour {
     }
 }
 //esta clase servira para poder almacenar en una estructura los datos el xml, no seran las clases cuyas instancias estaran en la visualización
+//para esto esta calse no deberia heredar de MonoBehaviour
 
 //Asi mismo hay varis valores que se pueden tratar como dicionarios, casi siempre son dupetas, por ahora esto es suficiente
