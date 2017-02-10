44f9b10be89d594333fe80856b213a88c5498645
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2017-02-10 12:24:55 -0500

    agregados comentarios

--- Assets/Scripts/AplicationControler.cs
+++ Assets/Scripts/AplicationControler.cs
@@ -15,3 +15,4 @@ public class AplicationControler : MonoBehaviour {
 }
 //este script debe estar ligado al de carga del xml para poder recibir la lista d nodos y aristas
 //node no pertenece a unas instancion de un gameObject
+//en esta visualizacion, node.cs sera un script para cada gameobject Node, por lo tanto esta aplicacion, al momento de recuperar datos debera instanciar un objeto y alli llenar datos en los objetos instnaciados
