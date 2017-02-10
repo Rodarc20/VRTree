f68d13cbb905717e7779486a75b60a78ac8268cb
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2017-02-10 12:22:37 -0500

    primeros cambios

--- Assets/Scripts/AplicationControler.cs
+++ Assets/Scripts/AplicationControler.cs
@@ -5,7 +5,11 @@ using System.Collections.Specialized;
 
 public class AplicationControler : MonoBehaviour {
     List<Node> nodos;
+    List<Edge> aristas;
+    GameObject board;//espacio sobre el que estaran los nodos, que en cierto modo, este script estara en ese objeto
+
     void start(){
+         
 
     }
     void update(){
@@ -15,3 +19,6 @@ public class AplicationControler : MonoBehaviour {
 }
 //este script debe estar ligado al de carga del xml para poder recibir la lista d nodos y aristas
 //node no pertenece a unas instancion de un gameObject
+//reemplazar las listas por arboles
+
+//esta visualización, creaara nodos para para cada scprior node.cs, esos scripts no seran herencia de monobehaviour
