0a65143be156be7a6c257d057ad5a8881b74870e
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2017-02-10 00:12:31 -0500

    Agregada la clase que controlara toda la aplicación

---
+++ Assets/Scripts/AplicationControler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+public class AplicationControler : MonoBehaviour {
+    List<Node> nodos;
+    void start(){
+
+    }
+    void update(){
+
+    }
+
+}
+//este script debe estar ligado al de carga del xml para poder recibir la lista d nodos y aristas
+//node no pertenece a unas instancion de un gameObject
